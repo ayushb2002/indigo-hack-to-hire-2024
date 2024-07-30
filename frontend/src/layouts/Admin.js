0ed17c312c27f38f91bffe94a286a32d3f41c66f
Ayush Bansal <ayushb6732@gmail.com>
2024-07-30 18:05:42 +0530

    Removed unnecessary files

--- frontend/src/layouts/Admin.js
+++ frontend/src/layouts/Admin.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Switch, Redirect } from "react-router-dom";
 import AdminNavbar from "components/Navbars/AdminNavbar.js";
 import Sidebar from "components/Sidebar/Sidebar.js";
-import HeaderStats from "components/Headers/HeaderStats.js";
 import FooterAdmin from "components/Footers/FooterAdmin.js";
 import Dashboard from "views/admin/Dashboard.js";
 import Settings from "views/admin/Settings.js";
@@ -19,8 +18,6 @@ export default function Admin() {
       <Sidebar />
       <div className="relative md:ml-64 bg-blueGray-100">
         <AdminNavbar />
-        {/* Header */}
-        <HeaderStats />
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
           <Switch>
             <PrivateRoute path="/admin/dashboard" component={Dashboard} />
